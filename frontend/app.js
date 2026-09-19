2669edef394588ae3a7c719def147a7067d0bcb2
JoshuaDelosReyes-sketch <joshuadelosreyes251205@gmail.com>
2026-09-19 13:04:54 +0800

    Update API URL

--- frontend/app.js
+++ frontend/app.js
@@ -1,8 +1,8 @@
-const API_URL = "http://127.0.0.1:5000";
+const API_URL = "https://login-system-mu-pink.vercel.app/";
 
 // =========================
 // LOGIN
-// =========================
+// =========================vb  
 
 const loginForm = document.getElementById("loginForm");
 
